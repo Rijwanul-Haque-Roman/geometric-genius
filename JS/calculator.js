663705e1835819ddaa13eaff48ad72a796998b2d
Rijwanul Haque Roman <rizwanul210058@diit.edu.bd>
2024-02-13 12:47:07 +0600

    version one fully done with responsiveness

--- JS/calculator.js
+++ JS/calculator.js
@@ -21,7 +21,33 @@ function claculateTriangleArea() {
 function claculateRectangleArea() {
   const width = getValue("rectangle-width");
   const length = getValue("rectangle-length");
-
   const area = width * length;
   setValue("rectangle-area", area);
+}
+
+function claculateParallelogramArea() {
+  const base = getValue("parallelogram-base");
+  const height = getValue("parallelogram-height");
+  const area = base * height;
+  setValue("parallelogram-area", area);
+}
+
+function claculateRhombusArea() {
+  const d1 = getValue("rhombus-d1");
+  const d2 = getValue("rhombus-d2");
+  const area = 0.5 * d1 * d2;
+  setValue("rhombus-area", area);
+}
+
+function claculatePentagonArea() {
+  const side = getValue("pentagon-side");
+  const area = 1.72 * side ** 2;
+  setValue("pentagon-area", area);
+}
+
+function claculateElllipseArea() {
+  const major = getValue("ellipse-major");
+  const minor = getValue("ellipse-minor");
+  const area = 3.14156 * major * minor;
+  setValue("ellipse-area", area);
 }
